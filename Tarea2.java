ab51172d2c14ff7bcbe19030e99afebb6b5232e7
AlexanderTovar <alexandertovar2003@gmail.com>
2023-02-20 20:24:35 -0600

    first commit

---
+++ Tarea2.java
@@ -0,0 +1,16 @@
+
+
+public class Tarea2 {
+
+	public static void main(String[] args) {
+		String info = "Alexander Estudiante Mexicana";
+		
+		String cadena[]= info.split(" ");
+		
+		System.out.println("Nombre = " + cadena[0]);
+		System.out.println("Profesion = " + cadena[1]);
+		System.out.println("Nacionalidad = " + cadena[2]);
+		
+	}
+
+}
