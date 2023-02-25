b1af3d1b170a32b1cffc54c1fbd160a5d6b54296
AlexanderTovar <alexandertovar2003@gmail.com>
2023-02-25 10:26:22 -0600

    first commit

--- Tarea2.java
+++ Tarea2.java
@@ -1,16 +1,23 @@
+package Tareas;
 
+import java.util.Scanner;
 
-public class Tarea2 {
+public class Tarea2{
 
-	public static void main(String[] args) {
-		String info = "Alexander Estudiante Mexicana";
-		
-		String cadena[]= info.split(" ");
-		
-		System.out.println("Nombre = " + cadena[0]);
-		System.out.println("Profesion = " + cadena[1]);
-		System.out.println("Nacionalidad = " + cadena[2]);
-		
-	}
+  public static void main(String args[]){
 
+   Scanner texto = new Scanner(System.in);
+   String datos = "";
+   
+   System.out.print("Escribe de manera ordenada tu Nombre Profesion País : ");
+   datos = texto.nextLine();
+   
+   String datosM = datos.toUpperCase();
+   String cadena[] = datosM.split(" ");
+
+   System.out.println("Nombre: " + cadena[0]);
+   System.out.println("Profesión: " + cadena[1]);
+   System.out.println("Pais: " + cadena[2]);
+
+ }
 }
